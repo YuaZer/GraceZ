a6f6e272127cd9149384c0b0c283f37d19c967ab
YuaZer <56108426+YuaZer@users.noreply.github.com>
2023-02-05 16:37:12 +0800

    Add files via upload

--- src/main/java/io/github/yuazer/gracez/GraceZ.java
+++ src/main/java/io/github/yuazer/gracez/GraceZ.java
@@ -1,18 +1,22 @@
 package io.github.yuazer.gracez;
 
+import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
+import com.pixelmonmod.pixelmon.api.pokemon.stats.BattleStatsType;
+import com.pixelmonmod.pixelmon.entities.pixelmon.PixelmonEntity;
 import io.github.yuazer.gracez.GuiSetting.Window;
 import io.github.yuazer.gracez.Registry.ItemRegistry;
 import io.github.yuazer.gracez.Utils.ZUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Items;
-import net.minecraft.util.math.RayTraceResult;
-import net.minecraft.util.math.vector.Vector3d;
-import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Mod("gracez")
@@ -22,39 +26,47 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class GraceZ {
     public static final String MOD_ID = "gracez";
     public static final String MOD_AUTHOR = "YuaZer";
+    public static Entity interactEntity = null;
 
     public GraceZ() {
-        //FMLJavaModLoadingContext.get().getModEventBus()   获取MOD总线
+        FMLJavaModLoadingContext.get().getModEventBus();   //获取MOD总线
         ItemRegistry.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        //initWindow();
     }
 
+    //    @SubscribeEvent
+//    public static void onJoin(PlayerEvent.PlayerLoggedInEvent event) {
+//        PlayerEntity player = event.getPlayer();
+//        ZUtils.sendMsg(player, "Welcome!欢迎您！" + player.getDisplayName().getString() + "|isClient:" + (player.level.isClientSide ? "Client" : "Server"));
+//    }
     @SubscribeEvent
-    public static void onInit(FMLLoadCompleteEvent event) {
-        System.out.println("init咯咯咯");
-    }
-
-    @SubscribeEvent
-    public static void onJoin(PlayerEvent.PlayerLoggedInEvent event) {
-        PlayerEntity player = event.getPlayer();
-        ZUtils.sendMsg(player, "Welcome!欢迎您！" + player.getDisplayName().getString() + "|isClient:" + (player.level.isClientSide ? "Client" : "Server"));
-
-    }
-
-    @SubscribeEvent
-    public static void onRightClick(PlayerInteractEvent.RightClickItem event) {
-        PlayerEntity player = event.getPlayer();
-        if (!player.level.isClientSide && player.getMainHandItem().getItem() == Items.DIAMOND_SWORD) {
-            RayTraceResult rtr = player.pick(20, 0, false);
-            Vector3d vec = rtr.getLocation();
-            player.teleportTo(vec.x, vec.y, vec.z);
-            ZUtils.sendMsg(player, "§a飞咯飞咯~");
-            initWindow();
+    public static void onInput(InputEvent.KeyInputEvent event) {
+        //当按下T键的时候
+        if (event.getKey() == 84) {
+            PlayerEntity player = Minecraft.getInstance().player;
+            if (interactEntity instanceof PixelmonEntity) {
+                Pokemon pokemon = ((PixelmonEntity) interactEntity).getPokemon();
+                List<String> pokeInfo = new ArrayList<>();
+                pokeInfo.add("名称:" + pokemon.getLocalizedName());
+                pokeInfo.add("血量:" + pokemon.getHealth());
+                pokeInfo.add("个体值:");
+                pokeInfo.add("生命:" + pokemon.getStats().getIVs().getStat(BattleStatsType.HP) + "|攻击:" + pokemon.getIVs().getStat(BattleStatsType.ATTACK));
+                pokeInfo.add("防御:" + pokemon.getStats().getIVs().getStat(BattleStatsType.DEFENSE) + "|特防:" + pokemon.getIVs().getStat(BattleStatsType.SPECIAL_DEFENSE));
+                pokeInfo.add("特攻:" + pokemon.getStats().getIVs().getStat(BattleStatsType.SPECIAL_ATTACK) + "|速度:" + pokemon.getIVs().getStat(BattleStatsType.SPEED));
+                ZUtils.sendMsg(player, pokeInfo);
+            } else {
+                ZUtils.sendMsg(player, "这不是个宝可梦");
+            }
         }
+    }
+
+    @SubscribeEvent
+    public static void onRightClick(PlayerInteractEvent.EntityInteract event) {
+        interactEntity = event.getTarget();
     }
 
     public static void initWindow() {
         Window window = new Window(600, 600, "GraceZ Mod [1.16.5]");
         window.run();
-
     }
 }
